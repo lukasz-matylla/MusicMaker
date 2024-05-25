749a16ca40575020ddebe8a63611b86df1e4362e
Lukasz Matylla <lukasz.matylla@gmail.com>
2024-05-25 19:26:36 +0200

    Corrected partitioning error in chord progression generation

--- Tools/RandomExtensions.cs
+++ Tools/RandomExtensions.cs
@@ -5,18 +5,19 @@
         public static int[] Partition(this Random rand, int size, int parts)
         {
             var cuts = new List<int>();
-            for (var i = 0; i < parts; i++)
+            for (var i = 0; i < parts - 1; i++)
             {
                 cuts.Add(rand.Next(size));
             }
             cuts.Sort();
 
             var result = new int[parts];
-            for (var i = 0; i < parts - 1; i++)
+            result[0] = cuts[0];
+            for (var i = 1; i < parts - 1; i++)
             {
-                result[i] = cuts[i + 1] - cuts[i];
+                result[i] = cuts[i] - cuts[i - 1];
             }
-            result[parts - 1] = size - cuts[parts - 1];
+            result[parts - 1] = size - cuts[parts - 2];
 
             return result;
         }
